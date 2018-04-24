98935bd22d986b81dceb36fe8c88d16deda021f4
Unknown <fernandovgduarte@gmail.com>
2018-04-24 09:04:50 +0100

    Changes
    Post class updated;
    PostDAO created

---
+++ BookStoreProject/BookStoreBackend/src/main/java/com/everiscenters/bookstore/dao/PostDAO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.everiscenters.bookstore.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author fvitordu
+ */
+public class PostDAO {
+    private String jdbcURL;
+	private String jdbcUsername;
+	private String jdbcPassword;
+	private Connection jdbcConnection;
+	
+	public PostDAO(String jdbcURL, String jdbcUsername, String jdbcPassword) {
+		this.jdbcURL = jdbcURL;
+		this.jdbcUsername = jdbcUsername;
+		this.jdbcPassword = jdbcPassword;
+	}
+	
+	protected void connect() throws SQLException {
+		if (jdbcConnection == null || jdbcConnection.isClosed()) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				throw new SQLException(e);
+			}
+			jdbcConnection = DriverManager.getConnection(
+										jdbcURL, jdbcUsername, jdbcPassword);
+		}
+	}
+	
+	protected void disconnect() throws SQLException {
+		if (jdbcConnection != null && !jdbcConnection.isClosed()) {
+			jdbcConnection.close();
+		}
+	}
+}

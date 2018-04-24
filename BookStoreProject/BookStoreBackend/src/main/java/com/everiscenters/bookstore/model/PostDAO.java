0fedf30318af0c8fb3c04954a45185b64c61276f
Unknown <fernandovgduarte@gmail.com>
2018-04-24 08:59:31 +0100

    PostDAOCreation

---
+++ BookStoreProject/BookStoreBackend/src/main/java/com/everiscenters/bookstore/model/PostDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.everiscenters.bookstore.model;
+
+/**
+ *
+ * @author fvitordu
+ */
+public class PostDAO {
+    
+}
